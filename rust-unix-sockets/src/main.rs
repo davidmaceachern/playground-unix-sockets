50cb46c4ea07b4cbc8643ee9c09adf5d73495db1
David MacEachern <david.maceachern.tech@gmail.com>
2020-11-16 16:17:55 +0100

    Writing to socket

--- rust-unix-sockets/src/main.rs
+++ rust-unix-sockets/src/main.rs
@@ -1,23 +1,48 @@
+use bytes::BytesMut;
 use serde_json::json;
 use std::io::prelude::*;
+use std::io::{self, Error, ErrorKind};
+use std::net::Shutdown;
 use std::os::unix::net::UnixStream;
-
+use std::str::from_utf8;
 fn main() -> std::io::Result<()> {
-    let mut stream = UnixStream::connect("/home/davidmaceachern/github/nodesozu/tmp/sock")?;
-    // TODO: identify how to send this message
-    let request = json!({
-    "id": "ID_TEST",
-    "version": 0,
-    "data": {
-      "type": "STATUS",
-    },
-    "worker_id": 0
-      });
 
-    let packaged = request.to_string();
-    stream.write_all(packaged.as_bytes())?;
+    let mut stream = UnixStream::connect("/home/davidmaceachern/github/nodesozu/tmp/sock")?;
+    //    stream.set_nonblocking(true).expect("Couldn't set nonblocking");
+    let CommandRequest = json!({
+    "id": "ID_CHAT",
+    "version": 0,
+    "type": "PROXY",
+    "data": {
+        "type": "STATUS"
+    }});
+    let mut buf = BytesMut::with_capacity(1024);
+    let message = serde_json::to_string(&CommandRequest)?;
+    println!("encoded message: {:?}", message);
+    buf.extend(message.as_bytes());
+    //println!("{:?}", buf);
+    buf.extend(&[0u8][..]);
+    //println!("buffercontent: {:?}", from_utf8(&buf[..]));
+    // Send message
+    match stream.write_all(&buf) {
+        Err(_) => panic!("couldn't send message"),
+        Ok(_) => println!("Sent"),
+    }
+    // TODO: create listener to receive this message? 
     let mut response = String::new();
-    stream.read_to_string(&mut response)?;
-    println!("{}", response);
+    let string = stream.read_to_string(&mut response)?;
+    println!("We received this as a response {}", string);
+    //    match stream.write_all(mut buf: &[u8]){
+    //        Err(_) => panic!("couldn't send message"),
+    //        Ok(_) => {}
+    //    }
+    //    let mut response = String::new();
+    //    println!("Receiving");
+    //    stream.read_to_string(&mut response)?;
+    //    println!("Printing");
+    //    println!("{}", response);
+    //    stream
+    //        .shutdown(Shutdown::Both)
+    //        .expect("shutdown function failed");
     Ok(())
 }
